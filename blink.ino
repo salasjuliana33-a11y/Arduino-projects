eff0478da6be013d641e3a03b14c253326d11930
salasjuliana33-a11y <salasjuliana33@gmail.com>
2026-07-25 18:22:52 -0500

    Add project documentation comments to blink.ino
    Added comments detailing project overview, components, and learnings.

--- blink.ino
+++ blink.ino
@@ -10,3 +10,10 @@ void loop() {
   digitalWrite(13,LOW);
   delay(500);
 }
+
+
+//Arduino_Projects This is my first harware electronics project. Circuit was built on a breadboard used to control the LED light using an arduino. 
+// This small project deomstrates the use of hardware/software, circuit design, and writing embedded code.
+//Component list:Arduino Uno, Breadboard, 1x red LED, 1x 330-ohm resistor,Jumperwires
+//What I Learned. BreadBoard Basics: Understanding how rows and power rails route electricity, Circuit Safety: How to use a current-limitng resistor to protect the LED
+// from burning out, Embedded Coding: Writing Arduino functions like 'pinMode()'; digitalWrite()'; and 'delay()'.
